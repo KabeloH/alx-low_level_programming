522ac5627aaede90a2adbc5d8c0ca367d3f56046
KabeloH <k.h.moabi@gmail.com>
2022-06-06 17:29:28 -0700

    script seven

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,13 +8,16 @@
  */
 int main()
 {
-	int a;
-	float b;
-	double c;
-	char d;
+	char a;
+	int b;
+	long c;
+	long long d;
+	float e;
 
-	printf("Size of int: %zu bytes\n", sizeof(a));
-	printf("Size of float: %zu bytes\n", sizeof(b));
-	printf("Size of double: %zu bytes\n", sizeof(c));
-	printf("Size of char: %zu byte\n", sizeof(d));
+	printf("Size of char: %zu bytes\n", sizeof(a));
+	printf("Size of int: %zu bytes\n", sizeof(b));
+	printf("Size of long: %zu bytes\n", sizeof(c));
+	printf("Size of long long: %zu byte\n", sizeof(d));
+	printf("Size of float: %zu byte\n", sizeof(e));
+	return (0)
 }
