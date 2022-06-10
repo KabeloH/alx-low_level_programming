dd3381bce1124b1d81f16d3c66882c0603950c5f
KabeloH <105058385+KabeloH@users.noreply.github.com>
2022-06-10 16:49:30 +0200

    alphabet

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * prototype of afunction that checks for uppercase alphabet.
+ * 
+ *Returns 1 if a alphabet is upprcase else returns 0
+ */
+int _isupper(int a)
+{
+	
+	if (isupper(a))
+	{
+        putchar(1); 
+	}
+    else
+    {
+    
+       putchar(0);
+	}
+
+}
