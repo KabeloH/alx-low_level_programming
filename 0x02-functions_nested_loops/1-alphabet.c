2e235899d222d0641d49bdbdf652d66652f4250d
KabeloH <k.h.moabi@gmail.com>
2022-06-12 10:13:32 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+
+
+/**
+ *
+ * print_alphabet - Check description
+ *
+ * Return: No return
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
