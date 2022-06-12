03817819a7db582f73c95d401bedb4e24890096f
KabeloH <k.h.moabi@gmail.com>
2022-06-12 09:03:16 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,19 @@
-int _putchar(char c);
+#include <unistd.h>
+
+
+
+/**
+ *
+ * _putchar function prints _putchar
+ *
+ *return 0 upon success
+ */
+
+int _putchar(char)
+
+{
+
+	write(1, "_putchar\n", 8);
+	
+	return(0);
+}
