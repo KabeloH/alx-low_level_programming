9f1e362a13cbb6d72b358ac0657f535f089b802d
KabeloH <105058385+KabeloH@users.noreply.github.com>
2022-06-12 18:46:31 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include <unistd.h>
 /*
- *
- * int main for entry point but void
- *
+ *int main for entry point but void
  * print _putchar and return 0
  */
-int main(void)
+int main(void) /*int main for entry point but void */
 {
 	write(1, "_putchar\n", 9);
-	
 	return (0);
 }
