44865494c5e0c028136a6feaf9618ca0aaa7c6dc
KabeloH <105058385+KabeloH@users.noreply.github.com>
2022-06-12 19:38:27 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,16 @@
 /**
  * main - prints out _putchar and a new line \n
  * Return: 0 on success
+ *_put char - print a single character
+ * Returns: a on success
  */
 int main(void)
 {
 	write(1, "_putchar\n", 9);
 	return (0);
+}
+
+int _putchar(char a)
+{
+	return (write(1, a, 1));
 }
